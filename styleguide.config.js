d61dd73749ba8842e97945a541fab00a03f9a7d4
Egor Kalinichev <egork92@gmail.com>
2017-11-27 23:01:49 +0100

    Put generated styleguide into build folder

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,27 +1,28 @@
 // Only until https://github.com/styled-components/styled-components/issues/945#issuecomment-340166853 would be merged
-
-const defaultResolver = require("react-docgen").resolver.findAllExportedComponentDefinitions;
-const annotationResolver = require("react-docgen-annotation-resolver").default;
+/* eslint-disable import/no-extraneous-dependencies */
+const defaultResolver = require('react-docgen').resolver.findAllExportedComponentDefinitions;
+const annotationResolver = require('react-docgen-annotation-resolver').default;
+/* eslint-enable import/no-extraneous-dependencies */
 
 module.exports = {
   title: 'Dybr UI',
   sections: [
     {
       name: 'Introduction',
-      content: './styleguide/Readme.md'
+      content: './styleguide/Readme.md',
     },
     {
       name: 'UI Components',
-      components: './src/components/**/index.js'
-    }
+      components: './src/components/**/index.js',
+    },
   ],
 
-  styleguideDir: './styleguide/out',
+  styleguideDir: './build/styleguide',
 
   resolver: (ast, recast) => {
     const annotatedComponents = annotationResolver(ast, recast);
     const defaultComponents = defaultResolver(ast, recast);
 
     return annotatedComponents.concat(defaultComponents);
-  }
+  },
 };
