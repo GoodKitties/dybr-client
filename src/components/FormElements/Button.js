bb73d9471c062b2de2cf855456b5aa1aadf1f216
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-12-02 00:12:20 +0200

    Move form specific components to their own place

--- src/components/FormElements/Button.js
+++ src/components/FormElements/Button.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  border-radius: 3px;
   padding: 0.25em 1em;
   margin: 0 1em;
-  background: transparent;
-  color: palevioletred;
-  border: 2px solid palevioletred;
+  width: 200px;
+  background: white;
+  color: #333;
+  border-radius: 3px;
+  border: 2px solid #333;
 `;
 
 export default Button;
---
+++ src/components/FormElements/index.js
@@ -0,0 +1,13 @@
+// top navbar
+// put any components in a separate components folder
+
+import _Input from './Input';
+import _Button from './Button';
+
+export const Input = _Input;
+export const Button = _Button;
+
+export default {
+  Input,
+  Button,
+};
