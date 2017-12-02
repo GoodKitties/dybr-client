6b833785ee6fc917d3de34ea0b02e5b4912afd6f
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-12-02 13:51:42 +0200

    Add theme file with colors

---
+++ src/components/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    brand: '#de413a',
+    brandLight: '#f76961',
+    brandLighter: '#ff948e',
+    brandDark: '##bd221a',
+    brandDarker: '#960d06',
+    accent: '#76ac78',
+    accentDark: '#407145',
+    lightGray: '#929292',
+    darkGray: '#333333',
+    background: '#ffffff',
+  },
+};
+
+export default theme;
