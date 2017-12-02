c46d2fe672003b0976afc0c33f2632851ad2a218
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-12-02 13:52:19 +0200

    Use color theme on the button component

--- src/components/FormElements/Button.js
+++ src/components/FormElements/Button.js
@@ -1,13 +1,22 @@
+import theme from 'components/theme';
 import styled from 'styled-components';
+
+const { colors } = theme;
 
 const Button = styled.button`
   padding: 0.25em 1em;
   margin: 0 1em;
   width: 160px;
-  background: white;
-  color: #333;
+  color: ${props => (props.primary ? colors.background : colors.darkGray)};
   border-radius: 3px;
-  border: 2px solid #333;
+  border: 2px solid ${props => (props.primary ? colors.brand : colors.darkGray)};
+  background: ${props => (props.primary ? colors.brand : 'transparent')};
 `;
+
+// We're extending Button with some extra styles
+/* const TomatoButton = Button.extend`
+color: tomato;
+border-color: tomato;
+`; */
 
 export default Button;
