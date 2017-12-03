55e5733a0e82009e5900b45b78261897b26bb0e3
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2017-12-03 16:55:48 +0200

    Redirect always to SignUp scene if user is logged in and registration is not complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import 'materialize-css/dist/css/materialize.min.css';
+import { isLoggedIn } from 'services/auth/helpers';
 import scenes from './scenes';
-import PrivateRoute from './PrivateRoute';
 
 const {
   Blog, Main, Profile, SignUp,
 } = scenes;
 
-export const Application = () => {
+export const Application = ({ loggedIn, signUpComplete }) => {
+  if (loggedIn && !signUpComplete) {
+    return (
+      <Router>
+        <Route path="/" component={SignUp} />
+      </Router>
+    );
+  }
   return (
     <Router>
       <Switch>
-        <PrivateRoute path="/wizardry" component={SignUp} />
-        <Route path="/" component={Main} />
+        <Route exact path="/" component={Main} />
         <Route path="/blogs/:uri" component={Blog} />
         <Route path="/profiles/:uri" component={Profile} />
       </Switch>
@@ -21,4 +29,16 @@ export const Application = () => {
   );
 };
 
-export default Application;
+Application.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+  signUpComplete: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: isLoggedIn(state),
+    signUpComplete: false, // TODO
+  };
+};
+
+export default connect(mapStateToProps)(Application);
